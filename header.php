91613be0d976ee97551c3b6d1c9c59a12218b8bf
Igor Soares <igorjsoares@gmail.com>
2019-11-15 19:45:43 +0000

    inclusão do link painel

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
      <li>
-        <button type="button" href="https://api.chatpro.com.br/painel" class="btn btn-warning">Acessar o PAINEL</button>
+        <a type="button" href="https://api.chatpro.com.br/painel" class="btn btn-warning">Acessar o PAINEL</a>
      </li>
     </ul>
   </nav>
