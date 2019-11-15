ba531b2cbd39f27716645dead988a2d56a560cff
Igor Soares <igorjsoares@gmail.com>
2019-11-15 19:43:41 +0000

    iNSREINDO TEXTO NO HEADER 2

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>      
       <li class="nav-item d-none d-sm-inline-block">
-        <H5>DOCUMENTAÇÃO</H5>
+        DOCUMENTAÇÃO
       </li>
     </ul>
 
