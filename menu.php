4832d44ad848439deca99b86db770063ed44de1d
Igor Soares <igorjsoares@gmail.com>
2019-11-15 19:51:02 +0000

    Alterações na logo 6

--- menu.php
+++ menu.php
@@ -2,7 +2,9 @@
   <aside class="main-sidebar sidebar-light-warning elevation-4">
     <!-- Brand Logo -->
     <a href="https://api.chatpro.com.br" class="brand-link">
-      <img src="dist/img/api/logo.png" alt="API ChatPro" class="brand-image" style="opacity: .8">
+      <img src="dist/img/api/logo.png" alt="API ChatPro" class="brand-image"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light"></span>
     </a>
 
     <!-- Sidebar -->
