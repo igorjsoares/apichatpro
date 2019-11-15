7574d2c203b02f07d54a5ad89402bcb71bba9d51
Igor Soares <igorjsoares@gmail.com>
2019-11-15 19:42:34 +0000

    iNSREINDO TEXTO NO HEADER 2

--- header.php
+++ header.php
@@ -8,9 +8,9 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>      
-      <li>
-        DOCUMENTAÇÃO
-      </li> 
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="" class="nav-link">DOCUMENTAÇÃO</a>
+      </li>
     </ul>
 
     <!-- Right navbar links -->
