030983e9bcc2f31ea8dc0d7541345d0cb0725843
Igor Soares <igorjsoares@gmail.com>
2019-11-15 22:03:39 +0000

    Alteração do footer

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <!-- /.content-wrapper -->
   <footer class="main-footer text-sm">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2019-2020.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.0
+      <b>Version</b> 1.0.0
     </div>
   </footer>
 
