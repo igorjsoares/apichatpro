86759878d60e186a5461b82ac237b0264b49f0c1
Igor Soares <igorjsoares@gmail.com>
2019-11-15 22:50:02 +0000

    Menu completo icones 9

--- menu.php
+++ menu.php
@@ -15,7 +15,7 @@
                with font-awesome or any other icon font library -->
           <li class="nav-item has-treeview menu-open">
             <a href="#" class="nav-link active">
-              <i class="far fa-comments"></i>
+              <i class="nav-icon far fa-comments"></i>
               <p>
                 A API
                 <i class="right fas fa-angle-left"></i>
