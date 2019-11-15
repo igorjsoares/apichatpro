afcbc0a87ebdd68c73c5205c24745dd7966c57ce
Igor Soares <igorjsoares@gmail.com>
2019-11-15 19:45:11 +0000

    inclusão do link painel

--- header.php
+++ header.php
@@ -9,14 +9,14 @@
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>      
       <li class="nav-item d-none d-sm-inline-block">
-        DOCUMENTAÇÃO
+        <a href="" class="nav-link">DOCUMENTAÇÃO</a>
       </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
      <li>
-        <button type="button" class="btn btn-warning">Acessar o PAINEL</button>
+        <button type="button" href="https://api.chatpro.com.br/painel" class="btn btn-warning">Acessar o PAINEL</button>
      </li>
     </ul>
   </nav>
