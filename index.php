845792fad9ebc328bdf675763393a2ffe1e0ee20
Igor Soares <igorjsoares@gmail.com>
2019-11-16 00:08:52 +0000

    CriaçAo do conteudo index 11

--- index.php
+++ index.php
@@ -48,6 +48,7 @@ include 'menu.php';
         <div class="">
             <iframe width="852" height="480" class="embed-responsive-item" src="https://isempreendimentos.site/apitutorials/videos/API_Apresentacao_ExportWS.mp4" frameborder="0" allowfullscreen=""></iframe>
         </div>
+        <BR>
 
         <div class="col-md-12">
           <div class="box box-solid">
@@ -60,6 +61,16 @@ include 'menu.php';
               <blockquote>
                 <p>API é um conjunto de rotinas e padrões ara acesso de dados de um aplicativo ou plataforma, é utilizada para comunicação entre sistemas, aplicativos ou plataformas. </p>
               </blockquote>
+
+                <p>A API ChatPro é um conjunto de métodos que irá fazer com que o seu sistema, aplicativo ou plataforma ENVIE e RECEBA mensagens via Whatsapp.</p>
+
+                <p>Com a API ChatPro seu sistema poderá de maneira automática ou não, enviar mensagens para clientes informando por exemplo o detalhamento de uma compra, podendo lembrar o paciente de uma consulta marcada no seu sistema, podendo enviar para o WhatsApp mensagens de rastreamento de encomentas, controle de logística, automação residencial e industrial, rastramento veicular entre várias outras possibilidades que podem ser alcançadas.</p>
+
+                <p>Seu sistema será capaz de receber também as respostas e fazer o que você quiser, você pode responder o cliente, dependendo da fala dele, pode salvar em banco de dados, pode alterar o status de uma transação... dentre várias possibilidades.</p>
+<BR>
+<h2>São muitas as possibilidades, invista na sua criatividade e deixe a comunicação com a gente.</h2>
+<BR>
+
             </div>
             <!-- /.box-body -->
           </div>
