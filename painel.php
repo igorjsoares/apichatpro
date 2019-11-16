90e83852eace2559b72d45f8e80884e1aac497e2
Igor Soares <igorjsoares@gmail.com>
2019-11-16 01:56:44 +0000

    Incluindo arquivos da arvore conceitos 2

--- painel.php
+++ painel.php
@@ -48,32 +48,9 @@ include 'menu.php';
           <div class="box box-solid">
             <!-- /.box-header -->
             <div class="box-body">
-            <div class="callout callout-success">
-                <h4>Testes grátis</h4>
-
-                <p>Para que tenha total segurança na nossa ferramenta, disponibilizamos 15 dias e 100 créditos grátis para testes.</p>
-              </div>
-
-                <p>Para testar siga os passos abaixo:</p>
-              
-                <ol>
-                    <li>Acesse <a href="http://api.chatpro.com.br">api.chatpro.com.br</a></li>
-                    <li>Clique no botão <strong>Teste grátis</strong></li>
-                    <li>Preencher os dados corretamente, é importante colocar os dados corretamente, pois <strong> senha será enviada para WhatsApp do número informado </strong> e as notificações importantes para o e-mail informado.</li>
-                    <li>Ao receber a senha por WhatsApp, clique no botão <strong>Login</strong> ou entre no endereço <a href="http://api.chatpro.com.br/painel">api.chatpro.com.br/painel</a></li>
-                    <li>Coloque seu e-mail e a senha recebida.</li>
-                </ol>
-                <p>Esse é o primeiro passo para começar a utilizar a API, nos próximos tópicos explicaremos cada parte do painel, mas se já quiser efetuar um teste, siga os passos abaixo:</p>
-                
-                <ol>
-                    <li>Abra no seu celular o aplicativo WhatsApp, em ajustes acesse WhatsApp web e leia o QRCode que está na tela do painel.</li>
-                    <li>Espere alguns segundos e seu painel aparecerá conectado (caso demore mais de 1 minuto, atualize a página).</li>
-                    <li>Com o status conectado, desça a barra de rolagem até o final, verá um campo de número é um campo de mensagem.</li>
-                    <li>Coloque um número com DDD (que não seja o número conectado), coloque uma mensagem e clique em <strong>Enviar mensagem</strong>.</li>
-                </ol>
-
-                <p>Note que a mensagem será enviada no seu aparelho e o painel te retornará uma resposta da requisição, entre outras coisas, com ID da mensagem e status.</p>
-                <p>Caso clique em <strong>Perfil</strong>, o painel irá executar o método /get_profile Que retorna para sua aplicação os dados do número inserido, inclusive a foto de  perfil (caso esses dados estejam disponíveis pro seu número)</p>
+                <p>O painel é onde você tem o controle dos seus créditos, consumo e instâncias. </p>
+                <p>Na barra superior tem acesso aos créditos da conta, e pode Comprar Créditos ou ver Histórico, tanto de pagamentos, quanto de consumo.</p>
+                <p>Mais abaixo temos as instâncias ativas  na conta, todas as instâncias que estiverem aqui, compartilharão o mesmo pacote de créditos. </p>
             </div>
             <!-- /.box-body -->
           </div>
--- preco.php
+++ preco.php
@@ -48,32 +48,19 @@ include 'menu.php';
           <div class="box box-solid">
             <!-- /.box-header -->
             <div class="box-body">
+
+            <p>A API trabalha com o modelo de pacotes de créditos pré pago, onde cada crédito representa uma mensagem enviada, ou seja, não cobramos por mensagens recebidas.</p>
+            <p>Quanto maior o pacote de créditos adquirido, menor o valor pago por crédito, podendo chegar a 4 centavos cada crédito, abaixo uma tabela com os valores:</p>
+            <ul>
+                <li>2.500 créditos • R$325,00 (R$0,13 por crédito)</li>
+                <li>5.000 créditos • R$450,00 (R$0,09 por crédito)</li>
+                <li>10.000 créditos • R$600,00 (R$0,06 por crédito)</li>
+                <li>50.000 créditos • R$2.000,00 (R$0,04 por crédito)</li>
+            </ul>
             <div class="callout callout-success">
-                <h4>Testes grátis</h4>
-
-                <p>Para que tenha total segurança na nossa ferramenta, disponibilizamos 15 dias e 100 créditos grátis para testes.</p>
-              </div>
-
-                <p>Para testar siga os passos abaixo:</p>
-              
-                <ol>
-                    <li>Acesse <a href="http://api.chatpro.com.br">api.chatpro.com.br</a></li>
-                    <li>Clique no botão <strong>Teste grátis</strong></li>
-                    <li>Preencher os dados corretamente, é importante colocar os dados corretamente, pois <strong> senha será enviada para WhatsApp do número informado </strong> e as notificações importantes para o e-mail informado.</li>
-                    <li>Ao receber a senha por WhatsApp, clique no botão <strong>Login</strong> ou entre no endereço <a href="http://api.chatpro.com.br/painel">api.chatpro.com.br/painel</a></li>
-                    <li>Coloque seu e-mail e a senha recebida.</li>
-                </ol>
-                <p>Esse é o primeiro passo para começar a utilizar a API, nos próximos tópicos explicaremos cada parte do painel, mas se já quiser efetuar um teste, siga os passos abaixo:</p>
-                
-                <ol>
-                    <li>Abra no seu celular o aplicativo WhatsApp, em ajustes acesse WhatsApp web e leia o QRCode que está na tela do painel.</li>
-                    <li>Espere alguns segundos e seu painel aparecerá conectado (caso demore mais de 1 minuto, atualize a página).</li>
-                    <li>Com o status conectado, desça a barra de rolagem até o final, verá um campo de número é um campo de mensagem.</li>
-                    <li>Coloque um número com DDD (que não seja o número conectado), coloque uma mensagem e clique em <strong>Enviar mensagem</strong>.</li>
-                </ol>
-
-                <p>Note que a mensagem será enviada no seu aparelho e o painel te retornará uma resposta da requisição, entre outras coisas, com ID da mensagem e status.</p>
-                <p>Caso clique em <strong>Perfil</strong>, o painel irá executar o método /get_profile Que retorna para sua aplicação os dados do número inserido, inclusive a foto de  perfil (caso esses dados estejam disponíveis pro seu número)</p>
+                <h4>DICA</h4>
+                <p>Os créditos não possuem data de validade, ou seja, não precisa ficar preocupado em consumir em um determinado espaço de tempo.</p>
+            </div>
             </div>
             <!-- /.box-body -->
           </div>
