87eb75b5418adeacd3a6275525c1c295301d63b7
Igor Soares <igorjsoares@gmail.com>
2019-11-17 16:39:11 +0000

    Texto instancia

--- instancias.php
+++ instancias.php
@@ -54,7 +54,10 @@ include 'menu.php';
               <p>Cada instância é individual, tanto em infraestrutura quanto em credenciais de envio, ou seja só compartilham o mesmo pacote de créditos.</p>
               <p>Ao realizar uma recarga você terá 1 instância inclusa no seu painel, para instâncias adicionais, cada instância tem o custo mensal de R$ 50,00.</p>
               <p>Para solicitar instâncias adicionais, basta solicitar um link de pagamento ao suporte, informando a quantidade de instâncias adicionais necessita.</p>
+                <BR>
                 <h4>SITUAÇÃO</h4>
+                <BR>
+                <div><img src="dist/img/api/tela_instancia_situacao.png" width="90%" alt=""></div>
                 <p>Neste espaço aparecerá o status da conexão do servidor da instância com o WhatsApp.</p>
                 <div class="callout callout-danger">
                 <p>Importante lembrar que para que a API funcione, o WhatsApp deve estar sincronizado com sua instância, atraves do QRcode.</p>
@@ -62,7 +65,27 @@ include 'menu.php';
             <p><h7>O WhatsApp não permite mais de uma conexão externa simultânea, sendo assim, não é possível estar conectado na API e acessar o Web Whatsapp.</h7></p>
               <p>Sugerimos que antes de sincronizar o número ao QrCode, acesse os ajustes do Whatsapp no aparelho > Whatsapp web > Sair de todas as sessões. Assim poderá ter uma conexão unica com o servidor.</p>
               <p>Para ler o QRCode acesse ajustes do seu aplicativo > Whatsapp Web > Escanear código QrCode (ou sinal de +).</p>
-              <p>Após sincronizar o whatsapp, lendo o QRcode, aguarde até o status da sua instância mudar para Conectado e ficar na cor verde (caso demore mais de 1 munitor, basta atualizar a pagina F5). No momento que sincroniza, o painel fez uma requisição (status) que trará as informações atuais da conexão, então, juntamente com a alteração de status, o painel traz na tela os dados do celular conectado como por exemplo bateria e rede. </p>
+              <p>Após sincronizar o whatsapp, lendo o QRcode, aguarde até o status da sua instância mudar para <strong style="green">Conectado</strong> e ficar na cor verde (caso demore mais de 1 munitor, basta atualizar a pagina F5). No momento que sincroniza, o painel fez uma requisição (status) que trará as informações atuais da conexão, então, juntamente com a alteração de status, o painel traz na tela os dados do celular conectado como por exemplo bateria e rede. </p>
+              
+              <BR>
+              <h4>CREDENCIAIS</H4>
+              <BR>
+              <div><img src="dist/img/api/tela_instancia_credenciais.png" width="90%" alt=""></div>
+              <p>Esse é o espaço destinado às informações da instância, como:</p>
+              <ul>
+              <li><strong>Código: </strong>Esse é o código único da sua instância.</li>
+              <li><strong>EndPoint: </strong>Esse é o prefixo do endereço que utilizará para acessar os métodos, cada instância tem um endereço diferente.</li>
+              <li><strong>Token: </strong>Token é uma espécie de senha para acessar os métodos, deverá ser colocado no header da requisição, com Authorization, somente esse token combinado com o endpoint, mostrará ao servidor que é essa intância que está solicitando o método, portanto não divulgue o seu token.</li>
+              <li><strong>Nome da instância: </strong>É o nome que aparecerá no painel, para controle pessoal das instâncias.</li>
+              <li><strong>Webhook: </strong>Webhook é o endereço de um arquivo no seu servidor que receberá as requisições que o Whatsapp enviará, nessas requisições vem, entre outras coisas como status da mensagem, se foi lida ou não, vem também a mensagem recebida. A partir daí você pode tratar ela, por exemplo salvando em banco de dados para visualização.</li>
+              <li><strong>Documentação interativa: </strong>Nesse botão você encontrará a documentação no site Swagger, onde poderá realizar testes diretamente na documentação.</li>
+              <li><strong>Código: </strong>Esse é o código único da sua instância.</li>
+              <li><strong>Testes (Caso conectado): </strong>Nesta área são realizados os primeios testes, o próprio painel chama as requisições. <strong style="color: red;"> ESTE ESPAÇO SÓ APARECERÁ OU FUNCIONARÁ COM O WHATSAPP SINCRONIZADO. </strong>
+                <li>Para testar, basta colocar um número com DDD (que não seja o número sincronizado), inserir uma mensagem e clicar em <strong>Enviar mensagem</strong>.</li>
+                <li>O painel fará a requisição send_message com os dados informados e mostrará na tela o retorno da requisição.</li>
+                <p>Para ver dados do perfil do número inserido, basta clicar em <strong>Perfil</strong> o painel irá fazer a requisição ?profile?</p>
+              </li>
+              </ul>
               </div>
             <!-- /.box-body -->
           </div>
